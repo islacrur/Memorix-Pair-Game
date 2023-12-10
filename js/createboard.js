cc1b4373286f1aa422e8e6a60b2a1c03c5a985b7
Claudia González <claudia.glezg@gmail.com>
2023-12-10 19:00:54 +0100

    new html and css with the sound feature and old files deleted

--- js/createboard.js
+++
@@ -1,6 +0,0 @@
-let newDiv = document.createElement("div");
-newDiv.className = "board";
-let newBoardRow = document.createElement("div");
-newBoardRow.className = "board_row";
-let cardButton = document.createElement("button");
-cardButton.className = "card";
--- js/tablero05.js
+++
@@ -1,6 +0,0 @@
-var card = document.querySelectorAll('.card');
-card.forEach(card=>
-card.addEventListener( 'click', function() {
-  card.classList.toggle('is-flipped');
-})
-);
